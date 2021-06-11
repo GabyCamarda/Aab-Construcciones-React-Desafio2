bc0120fbeb61aa04f826bed1b93c3d0f92af2daf
GabyCamarda <gabrielcamardarojas@gmail.com>
2021-06-10 23:37:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		let name = 'Claudio';
+		return (
+			<div>
+				<h1>Aab Construcciones</h1>
+				<h2>Bienvenido {name}</h2>
+				<ul>
+					<li>Inicio</li>
+					<li>Nosotros</li>
+					<li>Servicios</li>
+					<li>Contactos</li>
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
